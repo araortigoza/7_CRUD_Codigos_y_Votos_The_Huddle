9d9c8887e5691a0190c44393dbb50489e2ee3256
araortigoza <araprtogoza17@gmail.com>
2026-02-16 11:59:00 -0300

    chore: comentarios explicativos y cambios menores

--- controladores/temaControlador.js
+++ controladores/temaControlador.js
@@ -1,48 +1,48 @@
-const temaModelo = require('../modelos/temaModelo');
+const temaModelo = require('../modelos/temaModelo'); // IMPORTAMOS TODAS LAS FUNCIONES DE MODELOS
 
+// CONTROLADOR PARA MOSTRAR TODOS LOS TEMAS EN EL NAVEGADOR
 const mostrarTemas = async (req, res) => {
-    const temas = await temaModelo.obtenerTodosTemas();
-    res.render('index', { temas });
+  const temas = await temaModelo.obtenerTodosTemas(); // SE COMUNICA CON MODELOS PARA OBTENER TODOS LOS TEMAS
+  res.render('index', { temas }); // RENDERIZAMOS A LA VISTA INDEX Y LE PASAMOS TODOS LOS TEMAS DEVUELTOS POR MODELOS
 };
 
+// CONTROLADOR PARA MOSTRAR EN FORMULARIO DE CREAR
 const mostrarFormularioCrear = (req, res) => {
-    res.render('crear');
+  res.render('crear'); // RENDERIZAMOS A LA VISTA CREAR
 };
 
+// CONTROLADOR PARA CREAR UN TEMA NUEVO
 const crearTema = async (req, res) => {
-    const { titulo, enlace } = req.body;
-    await temaModelo.crearTema(titulo, enlace);
-    res.redirect('/');
+  const { titulo } = req.body; // SE EXTRAE EL TITULO DE LA REQUEST
+  await temaModelo.crearTema(titulo); // SE COMUNICA CON EL MODELO PARA CREAR NUEVO TEMA PASANDOLE EL TITULO EXTRAIDO
+  res.redirect('/'); // NOS REDIRIJIMOS A LA PAGINA PRINCIPAL
 };
 
-const mostrarFormularioEditar = async (req, res) => {
-    const temas = await temaModelo.obtenerTodosTemas();
-    const tema = temas.find( t => t.id === parseInt(req.params.id));
-    res.render('editar', { tema });
-};
-
+// CONTROLADOR PARA ACTUALIZAR UN TEMA
 const actualizarTema = async (req, res) => {
-    const { titulo, enlace } = req.body;
-    await temaModelo.actualizarTema(req.params.id, titulo, enlace);
-    res.redirect('/');
+  const { titulo } = req.body; // SE EXTRAE EL TITULO EDITADO DE LA REQUEST
+  await temaModelo.actualizarTema(req.params.id, titulo); // SE COMUNICA CON EL MODELO PARA ACTUALIZAR UN TEMA PASANDOLE LOS ARGUMENTOS NECESARIOS
+  res.json({ ok: true }); // ENVIAMOS UNA CONFIRMACION
 };
 
+// CONTROLADOR PARA ELIMINAR UN TEMA
 const eliminarTema = async (req, res) => {
-    await temaModelo.eliminarTema(req.params.id);
-    res.redirect('/');
+  await temaModelo.eliminarTema(req.params.id);  // SE COMUNICA CON EL MODELO PARA ELIMINAR UN TEMA Y LE PASAMOS EL ARGUMENTO NECESARIO EXTRAYENDOLO DE LA REQUEST
+  res.json({ ok: true }); // ENVIAMOS UNA CONFIRMACION
 };
 
+// CONTROLADOR PARA VOTAR UN TEMA
 const votarTema = async (req, res) => {
-    await temaModelo.votarTema(req.params.id);
-    res.json({ ok: true});
+  await temaModelo.votarTema(req.params.id); // SE COMUNICA CON EL MODELO PARA VOTAR UN TEMA PASANDOLE LOS PARAMETROS NECESARIOS EXTRAYENDOLO DE LA REQUETS
+  res.json({ ok: true }); // ENVIAMOS UNA CONFIRMACION
 };
 
+// EXPORTAMOS TODAS LAS FUNCIONES PARA UTILIZARLOS EN OTROS ARCHIVOS
 module.exports = {
-    mostrarTemas,
-    mostrarFormularioCrear,
-    crearTema,
-    mostrarFormularioEditar,
-    actualizarTema,
-    eliminarTema,
-    votarTema
+  mostrarTemas,
+  mostrarFormularioCrear,
+  crearTema,
+  actualizarTema,
+  eliminarTema,
+  votarTema
 };
